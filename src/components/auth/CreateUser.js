bbdc08407851986e916f16136a837062ba547d6b
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2023-03-21 00:09:23 +0100

    register + login

--- src/components/auth/CreateUser.js
+++ src/components/auth/CreateUser.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { authLogin } from '../../store/actions.js';
 import { registerUser } from '../../store/createUserActions';
 import Error from '../Error';
 
@@ -11,6 +12,7 @@ const CreateUser = () => {
   const dispatch = useDispatch();
 
   const { register, handleSubmit } = useForm();
+  const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,15 +21,21 @@ const CreateUser = () => {
     }
   }, [navigate, token]);
 
-  const submitForm = (data) => {
+  const submitForm = async (data) => {
     if (data.password !== data.confirmPassword) {
       setCustomError('el Password no coincide');
       return;
     }
-    data.email = data.email.toLowerCase();
+    const email = data.email.toLowerCase();
+    const password = data.password;
+    const rememberMe = true;
 
-    dispatch(registerUser(data));
+    await dispatch(registerUser(data));
+    await dispatch(authLogin({ email, password }, rememberMe));
+    const to = location.state?.from?.pathname || '/';
+    navigate(to, { replace: true });
   };
+
   return (
     <form onSubmit={handleSubmit(submitForm)} className="form-page-container">
       <h1 className="form-title"> {'Sign up'} </h1>
