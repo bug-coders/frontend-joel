d9e68682b6226a1056f0efd9743c5a375536e69a
Angel Magallanes <angelmagallanes80@gmail.com>
2023-03-21 10:58:57 +0100

    estilos de creación de anuncios y creacion de usuarios

--- src/components/auth/CreateUser.js
+++ src/components/auth/CreateUser.js
@@ -1,11 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { registerUser } from '../../store/createUserActions';
-import Error from '../Error';
-import Button from '../Button';
-import "./LoginPage.css";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { registerUser } from "../../store/createUserActions";
+import Error from "../Error";
+import Button from "../Button";
+import logo from "../../assets/wakapop propio.png";
+import sax from "../../assets/sax.png";
+import guitt from "../../assets/ESP.png";
+import iconT from '../../assets/icons/Twitter.png';
+import iconF from '../../assets/icons/Facebook.png';
+import iconI from '../../assets/icons/Instagram.png';
+import "./CreatePage.css";
 
 const CreateUser = () => {
   const [customError, setCustomError] = useState(null);
@@ -17,13 +23,13 @@ const CreateUser = () => {
 
   useEffect(() => {
     if (token) {
-      navigate('/');
+      navigate("/");
     }
   }, [navigate, token]);
 
   const submitForm = (data) => {
     if (data.password !== data.confirmPassword) {
-      setCustomError('el Password no coincide');
+      setCustomError("el Password no coincide");
       return;
     }
     data.email = data.email.toLowerCase();
@@ -31,56 +37,88 @@ const CreateUser = () => {
     dispatch(registerUser(data));
   };
   return (
-    <form onSubmit={handleSubmit(submitForm)} className="form-page-container">
-      <h1 className="form-title"> {'Sign up'} </h1>
+    <div className="formMain">
+      
+        <div className="formCont">
+          <div className="logoCont">
+            <img className="logoReg" src={logo} alt="Wusikando" />
+          </div>
+          <div className="formcontainer">
+          <img className="imgAdd" src={sax} alt="Wusikando" />
+            <form
+              onSubmit={handleSubmit(submitForm)}
+              className="form-page-container-sign">
+              <h1 className="form-title-sign"> {"Sign up"} </h1>
 
-      <div className="form-group">
-        <input
-          type="text"
-          className="form-input"
-          placeholder={'Name'}
-          {...register('name')}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="email"
-          className="form-input"
-          placeholder={'Email'}
-          {...register('email')}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          className="form-input"
-          placeholder={'Password'}
-          {...register('password')}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          className="form-input"
-          placeholder={'Confirmar password'}
-          {...register('confirmPassword')}
-          required
-        />
-      </div>
-      {error && <Error>{error}</Error>}
-      {customError && <Error>{customError}</Error>}
-      <Button type="submit" className="button" disabled={loading}>
-        {loading ? 'Loading...' : 'Register'}
-      </Button>
-      <Button className="button">
-        <NavLink className="navlinks" to="/">
-           Volver 
-        </NavLink>
-      </Button>
-    </form>
+              <div className="form-group-sign">
+                <input
+                  type="text"
+                  className="form-input-sign"
+                  placeholder={"Name"}
+                  {...register("name")}
+                  required
+                />
+              </div>
+              <div className="form-group-sign">
+                <input
+                  type="email"
+                  className="form-input-sign"
+                  placeholder={"Email"}
+                  {...register("email")}
+                  required
+                />
+              </div>
+              <div className="form-group-sign">
+                <input
+                  type="password"
+                  className="form-input-sign"
+                  placeholder={"Password"}
+                  {...register("password")}
+                  required
+                />
+              </div>
+              <div className="form-group-sign">
+                <input
+                  type="password"
+                  className="form-input-sign"
+                  placeholder={"Confirmar password"}
+                  {...register("confirmPassword")}
+                  required
+                />
+              </div>
+              {error && <Error>{error}</Error>}
+              {customError && <Error>{customError}</Error>}
+              <Button type="submit" className="button-sign" disabled={loading}>
+                {loading ? "Loading..." : "Register"}
+              </Button>
+              <Button className="button-sign">
+                <NavLink className="navlinks" to="/">
+                  Volver
+                </NavLink>
+              </Button>
+            </form>
+            <img className="imgAdd" src={guitt} alt="Wusikando" />
+          </div>
+          <footer className="footer">
+        <div>
+          <a href="https://twitter.com/?lang=es" target="_blank" rel="noopener noreferrer">
+            <img className="iconFoot" src={iconT} alt="twitter logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <img className="iconFoot" src={iconI} alt="instagram" />
+          </a>
+        </div>
+        <div>
+          <a href="https://es-es.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <img className="iconFoot" src={iconF} alt="Facebook" />
+          </a>
+        </div>
+      </footer>
+        </div>
+      
+    </div>
   );
 };
 
