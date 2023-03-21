00e42c222d27ec15446c14be75e3b3abce16276a
Angel Magallanes <angelmagallanes80@gmail.com>
2023-03-21 12:04:51 +0100

    modificaciones de imagenes de registro y creacion de anuncios

--- src/components/adverts/Filters.js
+++ src/components/adverts/Filters.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getApiTags } from "../../store/selectors.js";
 import { apiTagsLoad } from "../../store/actions.js";
 import Button from "../Button";
+import filtr from "../../assets/filter_filters_funnel_list_navigation_sort_sorting_icon_123212.webp";
 
 const Filters = ({ getAdvertsFilter }) => {
   const [name, setName] = useState("");
@@ -70,7 +71,9 @@ const Filters = ({ getAdvertsFilter }) => {
   return (
     <form className="adverts-filters-container" onSubmit={handleSubmit}>
       <div className="adverts-filters">
-          <label className="labFilter" htmlFor="byName">Por nombre</label>
+        <label className="labFilter" htmlFor="byName">
+          Por nombre
+        </label>
         <div>
           <input
             type="text"
@@ -82,37 +85,37 @@ const Filters = ({ getAdvertsFilter }) => {
           />
         </div>
 
-        
-          <fieldset
-            className="filter-fieldset-radio"
-            onChange={handleChangeSale}
-            value={sale}>
-            <legend>Tipo de anuncio:</legend>
+        <fieldset
+          className="filter-fieldset-radio"
+          onChange={handleChangeSale}
+          value={sale}>
+          <legend>Tipo de anuncio:</legend>
 
-            <div className="radioCont">
-              <label className="radioLabel" htmlFor="bySell">
-                Venta
-              </label>
-              <input type="radio" name="bySell" id="bySell" value={true} />
-            </div>
+          <div className="radioCont">
+            <label className="radioLabel" htmlFor="bySell">
+              Venta
+            </label>
+            <input type="radio" name="bySell" id="bySell" value={true} />
+          </div>
 
-            <div className="radioCont">
-              <label className="radioLabel" htmlFor="byBuy">
-                Compra
-              </label>
-              <input type="radio" name="bySell" id="byBuy" value={false} />
-            </div>
-            <div className="radioCont">
-              <label className="radioLabel" htmlFor="All">
-                Todos
-              </label>
-              <input type="radio" name="bySell" id="All" value="" />
-            </div>
-          </fieldset>
-        
+          <div className="radioCont">
+            <label className="radioLabel" htmlFor="byBuy">
+              Compra
+            </label>
+            <input type="radio" name="bySell" id="byBuy" value={false} />
+          </div>
+          <div className="radioCont">
+            <label className="radioLabel" htmlFor="All">
+              Todos
+            </label>
+            <input type="radio" name="bySell" id="All" value="" />
+          </div>
+        </fieldset>
 
         <div className="byPrice">
-          <label className="labFilter" htmlFor="byPrice">Por precio</label>
+          <label className="labFilter" htmlFor="byPrice">
+            Por precio
+          </label>
           <input
             type="number"
             name="byPrice"
@@ -157,6 +160,96 @@ const Filters = ({ getAdvertsFilter }) => {
 
         <Button type="submit" /* disabled={isDisabled()} */>Filtrar</Button>
       </div>
+      <details className="lista-detalle-filtro">
+        <summary><h3>Filtros-</h3>
+          <img className="icontop" src={filtr} alt="Menu" />
+        </summary>
+        <ul>
+          <li>
+            <input
+              type="text"
+              name="byName"
+              id="byName"
+              placeholder="Por Nombre"
+              onChange={handleChangeName}
+              value={name}
+            />
+          </li>
+
+          <li>
+            <fieldset
+              className="filter-fieldset-radio"
+              onChange={handleChangeSale}
+              value={sale}>
+              <legend>Tipo de anuncio:</legend>
+
+              <div className="radioCont">
+                <label className="radioLabel" htmlFor="bySell">
+                  Venta
+                </label>
+                <input type="radio" name="bySell" id="bySell" value={true} />
+              </div>
+
+              <div className="radioCont">
+                <label className="radioLabel" htmlFor="byBuy">
+                  Compra
+                </label>
+                <input type="radio" name="bySell" id="byBuy" value={false} />
+              </div>
+              <div className="radioCont">
+                <label className="radioLabel" htmlFor="All">
+                  Todos
+                </label>
+                <input type="radio" name="bySell" id="All" value="" />
+              </div>
+            </fieldset>
+          </li>
+
+          <li>
+            <input
+              type="number"
+              name="byPrice"
+              id="byPriceMin"
+              placeholder="Precio mínimo"
+              onWheel={(event) => event.currentTarget.blur()}
+              onChange={handleChangeMinPrice}
+              value={minPrice}
+            />
+            <input
+              type="number"
+              name="byPrice"
+              id="byPriceMax"
+              placeholder="Precio máximo"
+              onWheel={(event) => event.currentTarget.blur()}
+              onChange={handleChangeMaxPrice}
+              value={maxPrice}
+            />
+          </li>
+          <li>
+          <select
+            name="byTags"
+            id="byTags"
+            style={{ padding: "20px" }}
+            multiple
+            onChange={handleChangeTags}
+            value={tags}>
+            <option key="none" value="" id="none">
+              ---
+            </option>
+            {apiTags.map((tag) => {
+              return (
+                <option key={tag} value={tag} id={tag}>
+                  {`${tag.charAt(0).toUpperCase() + tag.slice(1)}`}
+                </option>
+              );
+            })}
+          </select>
+          </li>
+          <li>
+          <Button type="submit" /* disabled={isDisabled()} */>Filtrar</Button>
+          </li>
+        </ul>
+      </details>
     </form>
   );
 };
